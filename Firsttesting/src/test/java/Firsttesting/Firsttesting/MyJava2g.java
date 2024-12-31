c1c7eff7702271f966a73ab980ca9c75e59e5f0f
syedy <syedy@DESKTOP-TAV9QK2.mynetworksettings.com>
2024-12-31 15:28:11 -0500

    first attempt

---
+++ Firsttesting/src/test/java/Firsttesting/Firsttesting/MyJava2g.java
@@ -0,0 +1,12 @@
+package Firsttesting.Firsttesting;
+
+public class MyJava2g {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello beautiful world");
+
+
+	}
+
+}
