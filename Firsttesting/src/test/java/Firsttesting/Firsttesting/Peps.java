f30fa5819fc55b9f207879b92edebff1932cde0b
syedy <syedy@DESKTOP-TAV9QK2.mynetworksettings.com>
2024-12-31 15:34:58 -0500

    try

---
+++ Firsttesting/src/test/java/Firsttesting/Firsttesting/Peps.java
@@ -0,0 +1,25 @@
+package Firsttesting.Firsttesting;
+
+import org.testng.annotations.Test;
+
+public class Peps {
+	
+	@Test
+	
+	
+	public void myMethod() {
+	
+		System.out.println("it worked");
+	
+	}
+	
+	
+	
+	@Test
+	public void myMethod2() {
+		
+		System.out.println("it worked well");
+}
+	
+} 
+
